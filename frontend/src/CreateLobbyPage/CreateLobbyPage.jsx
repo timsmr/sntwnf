8d3c5de3d2667d5dc6c10d4b6ef450d54dc83389
leshafatin <70691537+leshafatin@users.noreply.github.com>
2022-05-14 14:02:41 +0300

    added fetch method in create_lobby page

--- frontend/src/CreateLobbyPage/CreateLobbyPage.jsx
+++ frontend/src/CreateLobbyPage/CreateLobbyPage.jsx
@@ -1,32 +1,112 @@
-import React from 'react';
-import './CreateLobbyPage.scss';
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import "./CreateLobbyPage.scss";
 import GenerateRoomNumber from "../utils/GenerateRoomNumber";
 
 const CreateLobbyPage = () => {
-	return (
-		<div className="d-flex flex-column justify-content-center w-100 h-100">
-			<div className="d-flex flex-column justify-content-center align-items-center">
-				<div className="form">
-					<form>
-						<div className="mb-3">
-							<label htmlFor="name" className="form-label">Имя:</label>
-							<input type="text" className="form-control" id="name"/>
-						</div>
+  const [newlobby, setNewlobby] = useState({
+    lobbyName: "",
+    eventdate: "",
+    guestname: "",
+    email: "",
+    preferences: "",
+  });
 
-						<div className="mb-3">
-							<label htmlFor="presentClue" className="form-label">Подсказки для подарка</label>
-							<input type="text" className="form-control" id="presentClue"/>
-						</div>
+  function handleSubmit(e) {
+    e.preventDefault();
+    const json = JSON.stringify(newlobby);
 
+    // const requestOptions = {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: json,
+    // };
+    // let response = fetch("/api/lobby/", requestOptions).then((response) =>
+    //   console.log(response)
+    // );
+    axios
+      .post("http://localhost:8000/api/lobby/", json)
+      .then((resp) => console.log(resp))
+      .catch((err) => console.log(err.message));
+  }
 
-						<p>Номер комнаты: {GenerateRoomNumber()}</p>
-						<button type="submit" className="btn btn-primary">Submit</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="d-flex flex-column justify-content-center w-100 h-100">
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <div className="form">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="lobby_name" className="form-label">
+                Название лобби
+              </label>{" "}
+              <input
+                type="text"
+                className="form-control"
+                id="lobbyName"
+                value={newlobby.lobbyName}
+                onChange={(e) =>
+                  setNewlobby({ ...newlobby, lobbyName: e.target.value })
+                }
+              />{" "}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="date" className="form-label">
+                Дата мероприятия
+              </label>
+              <input
+                type="date"
+                className="form-control"
+                id="date"
+                value={newlobby.eventdate}
+                onChange={(e) =>
+                  setNewlobby({ ...newlobby, eventdate: e.target.value })
+                }
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Имя
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                value={newlobby.guestname}
+                onChange={(e) =>
+                  setNewlobby({ ...newlobby, guestname: e.target.value })
+                }
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Электронная почта
+              </label>
+              <input type="text" className="form-control" id="email" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="giftClue" className="form-label">
+                Подсказка
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="giftClue"
+                value={newlobby.preferences}
+                onChange={(e) =>
+                  setNewlobby({ ...newlobby, preferences: e.target.value })
+                }
+              />
+            </div>
+            <p> Номер комнаты: {GenerateRoomNumber()} </p>{" "}
+            <button onClick={handleSubmit} className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateLobbyPage;
-
--- frontend/src/LobbyJoinPage/LobbyJoinPage.jsx
+++ frontend/src/LobbyJoinPage/LobbyJoinPage.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
 const LobbyJoinPage = () => {
-	return (
-		<div className="d-flex flex-column justify-content-center w-100 h-100">
-			<div className="d-flex flex-column justify-content-center align-items-center">
-				<div className="form">
-					<h1 className="text-white my-3">Войти в лобби</h1>
-					<div className="input-group mb-3">
-						<input type="text" className="form-control" placeholder="Номер лобби"/>
-					</div>
-					<div className="input-group mb-3">
-						<input type="text" className="form-control" placeholder="Имя"/>
-					</div>
-						<div className="input-group mb-3">
-						<input type="text" className="form-control" placeholder="Предпочтения"/>
-					</div>
+  const [input, setInput] = useState("");
 
-				</div>
-			</div>
-		</div>
-	);
+  function handleEnter() {
+    console.log(input);
+    fetch(`http://127.0.0.1:8000/api/lobby/${input}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
+  return (
+    <div className="d-flex flex-column justify-content-center w-100 h-100">
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <div className="form">
+          <h1 className="text-white my-3">Войти в лобби</h1>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Номер лобби"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
+          <button className="btn btn-secondary" onClick={handleEnter}>
+            Войти
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LobbyJoinPage;
