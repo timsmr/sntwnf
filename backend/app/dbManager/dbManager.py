cdf067bc6c351bbf703609590bd143e278ba0afc
Kremon <kremoor@gmail.com>
2022-12-03 19:30:57 +0300

    docker games

--- backend/app/dbManager/dbManager.py
+++ backend/app/dbManager/dbManager.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from os import environ
-
-db_username = environ.get("DB_USER", "user")
-db_password = environ.get("DB_PASSWORD", "password")
-db_name = environ.get("DB_HOST", "localhost")
+db_username = environ.get("DB_USER", "postgres")
+db_password = environ.get("DB_PASSWORD", "Rjkzy231240")
+db_name = environ.get("DB_NAME", "santa")
 
 db_host = "localhost"
 
@@ -12,6 +11,7 @@ engine = create_engine(f'postgresql://{db_username}:{db_password}@{db_host}:5432
 
 
 session = Session(engine)
+
 
 
 
