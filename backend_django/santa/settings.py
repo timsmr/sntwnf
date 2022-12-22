59bf30199d0c368f5730cca6a89a530ba1a0514b
Kremon <kremoor@gmail.com>
2022-12-23 00:11:52 +0300

    docker games

--- backend_django/santa/settings.py
+++ backend_django/santa/settings.py
@@ -141,6 +141,8 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     config('ALLOWED_CORS', default="http://localhost:3000"),
+    "http://0.0.0.0:3000",
+    "http://127.0.0.1:3000",
 ]
 
 SIMPLE_JWT = {
