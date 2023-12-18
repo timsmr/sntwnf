b49590c3c4f9fc7ed4c0acd520023e65659b431d
root <root@2319624-jq80249.twc1.net>
2023-12-19 00:17:40 +0300

    change docker-compose and base url

--- frontend/src/api/apiService.ts
+++ frontend/src/api/apiService.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, AxiosRequestHeaders } from "axios";
 import * as T from "./types/types";
 
-const baseUrl = "http://sntwnf_backend_1:8000/api/";
+const baseUrl = "http://santa_backend:8000/api/";
 
 class ApiService {
   private axiosInstance!: AxiosInstance;
