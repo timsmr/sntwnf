0fdfac7836997f41e2b33f508e748aea86d061db
Timur Smirnov <smrtim17@gmail.com>
2023-12-18 19:32:41 +0300

    add modal window adaptive

--- frontend/src/shared/components/ModalWindow/index.tsx
+++ frontend/src/shared/components/ModalWindow/index.tsx
@@ -9,8 +9,8 @@ export const ModalWindow = ({setIsAgreed, setIsOpened}: I.ModalWindowProps) => {
       <div className={styles.hint}>
         <p className={styles.info}>фо риал?!</p>
         <div>
-          <Button onClick={() => {setIsAgreed(true); setIsOpened(false)}} className={styles.btnYes} label="да" buttonStyle="primary" />
-          <Button onClick={() => {setIsAgreed(false); setIsOpened(false)}} label="нет" buttonStyle="primary" />
+          <Button onClick={() => {setIsAgreed(true); setIsOpened(false)}} className={styles.btn} label="да" buttonStyle="primary" />
+          <Button onClick={() => {setIsAgreed(false); setIsOpened(false)}} className={styles.btn} label="нет" buttonStyle="primary" />
         </div>
       </div>
     </div>
